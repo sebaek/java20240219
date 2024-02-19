d29de87d330c10af88f237a4a2f772fb2c3f3e79
sebaek <sebaek@gmail.com>
2024-02-19 12:41:08 +0900

    처음 만들어본 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("밥 먹자!");
+        System.out.println("돈까스");
+        System.out.println("햄버거");
+        System.out.println("피자");
+        System.out.println("제육볶음");
+
+    }
+}
