11e04e7d49394156fc4b46776db0265ab97ec204
sebaek <sebaek@gmail.com>
2024-02-20 10:43:35 +0900

    char type

--- src/c02/lecture/p02type/C06Short.java
+++ src/c02/lecture/p02type/C06Short.java
@@ -1,14 +1,17 @@
 package c02.lecture.p02type;
 
 public class C06Short {
-    // short
-    // 2byte == 16bits
-    // -32768 ~ 32767
+    public static void main(String[] args) {
 
-    short a = 2342;
-    short b = -234;
-    short c = -32768;
+// short
+// 2byte == 16bits
+// -32768 ~ 32767
+
+        short a = 2342;
+        short b = -234;
+        short c = -32768;
 //    short d = -32769;
-    short d = 32767;
+        short d = 32767;
 //    short e = 32768;
+    }
 }
---
+++ src/c02/lecture/p02type/C07Character.java
@@ -0,0 +1,25 @@
+package c02.lecture.p02type;
+
+public class C07Character {
+    public static void main(String[] args) {
+        // char (character)
+        // 2byte == 16bits
+        // 0 ~ 65535
+        // unicode 
+
+        char a = 99;
+        char b = 120;
+        char c = 65535;
+//        char d = 65536; // x
+//        char e = -1; // x
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+
+        char f = 4314;
+
+        System.out.println("f = " + f);
+
+    }
+}
