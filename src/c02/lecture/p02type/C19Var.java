1524f5215aafc77d736b8a9f010e00294bbf0498
sebaek <sebaek@gmail.com>
2024-02-20 15:41:37 +0900

    var

--- src/c02/lecture/p02type/C19Var.java
+++ src/c02/lecture/p02type/C19Var.java
@@ -10,6 +10,7 @@ public class C19Var {
         // 따라서 변수의 선언과 할당을 동시에 해야함
         // 지역변수에서만 사용 가능 (필드, 파라미터, 리턴타입에서 사용 불가능)
 
+
         int a = 10;
         var b = 10; // b는 int로 추론됨
 
@@ -36,5 +37,7 @@ public class C19Var {
         var list3 = new ArrayList<Map<String, Integer>>();
 
 
+        // 아래코드 처럼 변수명으로 사용하지 말 것
+        int var = 3; // x
     }
 }
