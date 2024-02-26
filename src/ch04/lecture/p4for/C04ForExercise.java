af2449c34d370873d927bb0f2aecc91beaf61b87
sebaek <sebaek@gmail.com>
2024-02-26 11:29:52 +0900

    for 연습문제

---
+++ src/ch04/lecture/p4for/C04ForExercise.java
@@ -0,0 +1,60 @@
+package ch04.lecture.p4for;
+
+public class C04ForExercise {
+    public static void main(String[] args) {
+        /*
+        0
+        1
+        2
+        3
+        4
+         */
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
+        /*
+        1
+        2
+        3
+        4
+        5
+         */
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        /*
+        0
+        2
+        4
+        6
+        8
+         */
+        /*
+        2
+        4
+        6
+        8
+        10
+         */
+
+        /*
+        4
+        3
+        2
+        1
+        0
+         */
+
+        /*
+        5
+        4
+        3
+        2
+        1
+         */
+
+
+    }
+}
