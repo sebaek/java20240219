2512ae999c8ec38c574a05c9fb0dc29ffe84d52b
sebaek <sebaek@gmail.com>
2024-02-27 10:08:09 +0900

    확인문제 2번 switch expression

--- src/ch04/exercise/Exercise02.java
+++ src/ch04/exercise/Exercise02.java
@@ -4,17 +4,13 @@ public class Exercise02 {
     public static void main(String[] args) {
         String grade = "B";
 
-        int score1 = 0;
-        switch (grade) {
-            case "A":
-                score1 = 100;
-                break;
-            case "B":
+        int score1 = switch (grade) {
+            case "A" -> 100;
+            case "B" -> {
                 int result = 100 - 20;
-                score1 = result;
-                break;
-            default:
-                score1 = 60;
-        }
+                yield result;
+            }
+            default -> 60;
+        };
     }
 }
