938162a1e69671fe07b64b85c495c3bc79faa532
sebaek <sebaek@gmail.com>
2024-03-04 10:27:22 +0900

    split

--- src/ch05/exercise/Exercise03.java
+++ src/ch05/exercise/Exercise03.java
@@ -1,2 +1,18 @@
-package ch05.exercise;public class Exercise03 {
+package ch05.exercise;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        String a = "java";
+        String b = new String("java");
+        String c = "java";
+
+        System.out.println(System.identityHashCode(a));
+        System.out.println(System.identityHashCode(b));
+        System.out.println(System.identityHashCode(c));
+
+        System.out.println(a == c); // x
+        System.out.println(a.equals(c)); // ok
+    }
 }
