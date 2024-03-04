3a02f2fafd12b89a235229c75e46a905cddd1af4
sebaek <sebaek@gmail.com>
2024-03-04 14:15:14 +0900

    method

---
+++ src/ch06/lecture/p2method/C02Method.java
@@ -0,0 +1,31 @@
+package ch06.lecture.p2method;
+
+public class C02Method {
+
+}
+
+class C02Car {
+    // 필드들...
+
+    // 메소드들...
+    // 인스턴스가 가진 기능들
+
+    // 문법: 책(230쪽)
+    // 리턴타입명 메소드명(파라미터 목록들...) {
+    //    실행 코드들...
+    //    return 문 // 없어도됨
+    // }
+
+    // 메소드명 작성 관습
+    // lowerCamelCase
+    // 동사로 시작
+
+    void run() {
+    }
+
+    void setSpeed() {
+    }
+
+    void getName() {
+    }
+}
