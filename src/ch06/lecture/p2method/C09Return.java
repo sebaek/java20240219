49f1bfb701da0a6374ccdc1c3ed5947ef088bcf8
sebaek <sebaek@gmail.com>
2024-03-05 09:41:07 +0900

    return, return type

--- src/ch06/lecture/p2method/C09Return.java
+++ src/ch06/lecture/p2method/C09Return.java
@@ -13,6 +13,8 @@ class C09MyClass {
     void method1() {
         // return :
         // 1. 메소드 종료
+        // 2. 메소드가 호출된 곳으로 값 반환
+        //    이 때, 반환되는 값의 타입이 메소드의 타입과 일치해야함!
 
         System.out.println("statement 1");
         System.out.println("statement 2");
