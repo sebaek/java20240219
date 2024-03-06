41a600995a9069e5632b7f210498c894519cc574
sebaek <sebaek@gmail.com>
2024-03-06 11:48:34 +0900

    public, private

---
+++ src/ch06/lecture/p7access/C01AccessModifier.java
@@ -0,0 +1,10 @@
+package ch06.lecture.p7access;
+
+public class C01AccessModifier {
+    public static void main(String[] args) {
+        C01MyClass o1 = new C01MyClass();
+        o1.age = 30;
+//        o1.address = "seoul";
+    }
+}
+
