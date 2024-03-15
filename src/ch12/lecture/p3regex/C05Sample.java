11cf51a35da8a1e16751f8d971c3c0319a856de1
sebaek <sebaek@gmail.com>
2024-03-15 11:10:58 +0900

    정규표현식으로 변수명 패턴 연습

--- src/ch12/lecture/p3regex/C05Sample.java
+++ src/ch12/lecture/p3regex/C05Sample.java
@@ -5,7 +5,8 @@ public class C05Sample {
         // _, $, 영문대소문자, 숫자 조합
         // 단 숫자로 시작하면 안됨
 
-        String pattern = ""; // 작성해보세요
+//        String pattern = "[_$a-zA-Z][_$a-zA-Z0-9]+"; // 작성해보세요
+        String pattern = "^(?![0-9])[A-Za-z0-9_$]+$"; // 작성해보세요
         System.out.println("num".matches(pattern)); // true
         System.out.println("var7".matches(pattern)); // true
         System.out.println("class".matches(pattern)); // true
