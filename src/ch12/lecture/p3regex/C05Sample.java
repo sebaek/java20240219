5264a0364a9fb6c65ac71d6da527a33ceea70656
sebaek <sebaek@gmail.com>
2024-03-15 10:44:56 +0900

    연습

--- src/ch12/lecture/p3regex/C05Sample.java
+++ src/ch12/lecture/p3regex/C05Sample.java
@@ -1,4 +1,16 @@
 package ch12.lecture.p3regex;
 
 public class C05Sample {
+    public static void main(String[] args) {
+        // _, $, 영문대소문자, 숫자 조합
+        // 단 숫자로 시작하면 안됨
+
+        String pattern = ""; // 작성해보세요
+        System.out.println("num".matches(pattern)); // true
+        System.out.println("var7".matches(pattern)); // true
+        System.out.println("class".matches(pattern)); // true
+        System.out.println("PI".matches(pattern)); // true
+        System.out.println("MAX_VALUE".matches(pattern)); // true
+        System.out.println("7num".matches(pattern)); // false
+    }
 }
