c14a077782e8e6ab4f85a0df4cd747de31025dad
sebaek <sebaek@gmail.com>
2024-03-15 14:12:40 +0900

    generic type

---
+++ src/ch13/lecture/p1generic/C02GenericType.java
@@ -0,0 +1,16 @@
+package ch13.lecture.p1generic;
+
+public class C02GenericType {
+}
+
+class C02Box {
+    private Object item;
+
+    public Object getItem() {
+        return item;
+    }
+
+    public void setItem(Object item) {
+        this.item = item;
+    }
+}
