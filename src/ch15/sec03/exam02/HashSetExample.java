223acf3735758313ef2760fa97a873807b537edd
sebaek <sebaek@gmail.com>
2024-03-19 12:24:33 +0900

    iterator

--- src/ch15/sec03/exam02/HashSetExample.java
+++ src/ch15/sec03/exam02/HashSetExample.java
@@ -1,6 +1,7 @@
 package ch15.sec03.exam02;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class HashSetExample {
@@ -13,7 +14,21 @@ public class HashSetExample {
 
         System.out.println(set.size());
 
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+            if (element.equals("jsp")) {
+                iterator.remove();
+            }
+        }
+
+
         set.remove("jdbc");
         System.out.println(set.size());
+
+        for (String element : set) {
+            System.out.println(element);
+        }
     }
 }
