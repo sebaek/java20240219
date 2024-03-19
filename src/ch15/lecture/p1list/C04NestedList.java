630e56c07a441f508e88e99ee4129b40f5671c8c
sebaek <sebaek@gmail.com>
2024-03-19 09:34:20 +0900

    nested list

--- src/ch15/lecture/p1list/C04NestedList.java
+++ src/ch15/lecture/p1list/C04NestedList.java
@@ -1,5 +1,7 @@
 package ch15.lecture.p1list;
 
+import java.util.List;
+
 public class C04NestedList {
     public static void main(String[] args) {
         int[][] array = {
@@ -12,7 +14,12 @@ public class C04NestedList {
         System.out.println(array[2][2]); // 93
         System.out.println(array[1][0]); // 83
 
-        // 
+        //
+        List<List<Integer>> list;
+
+        System.out.println(list.get(1).get(1)); // 92
+        System.out.println(list.get(2).get(2)); // 93
+        System.out.println(list.get(1).get(1)); // 83
 
     }
 }
