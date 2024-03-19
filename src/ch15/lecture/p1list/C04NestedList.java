454dafcea3ddeb41073a46c3e4b635005d5f1b28
sebaek <sebaek@gmail.com>
2024-03-19 09:33:31 +0900

    nested list

--- src/ch15/lecture/p1list/C04NestedList.java
+++ src/ch15/lecture/p1list/C04NestedList.java
@@ -1,4 +1,18 @@
 package ch15.lecture.p1list;
 
 public class C04NestedList {
+    public static void main(String[] args) {
+        int[][] array = {
+                {95, 86},
+                {83, 92, 96},
+                {78, 83, 93, 87, 88}
+        };
+
+        System.out.println(array[1][1]); // 92
+        System.out.println(array[2][2]); // 93
+        System.out.println(array[1][0]); // 83
+
+        // 
+
+    }
 }
