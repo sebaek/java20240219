a5f1046cad133bd2d8f545ab262b9b1fb634d3c6
sebaek <sebaek@gmail.com>
2024-03-21 11:26:08 +0900

    of method : 수정할 수 없는 콜렉션 리턴

---
+++ src/ch15/lecture/p6of/C01List.java
@@ -0,0 +1,28 @@
+package ch15.lecture.p6of;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C01List {
+    public static void main(String[] args) {
+        //
+        List<String> list = new ArrayList<>();
+        list.add("java");
+        list.add("css");
+        list.add("html");
+        list.add("react");
+        System.out.println(list);
+        list.add("spring");
+        list.remove(0);
+        list.set(1, "vue");
+        System.out.println(list);
+
+        // of
+        List<String> list2 = List.of("java", "css", "html", "react");
+        System.out.println(list2);
+//        list2.set(1, "vue");
+//        list2.add("jsp");
+//        list2.remove(0);
+
+    }
+}
