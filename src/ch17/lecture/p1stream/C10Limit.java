7e96c12a1a49c363d778b03b661c225976548785
sebaek <sebaek@gmail.com>
2024-03-21 16:27:05 +0900

    limit

---
+++ src/ch17/lecture/p1stream/C10Limit.java
@@ -0,0 +1,44 @@
+package ch17.lecture.p1stream;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class C10Limit {
+    public static void main(String[] args) {
+        // limit : 주어진 수 만큼의 요소의 스트림을 리턴
+        List<Integer> list = List.of(8, 2, 5, 1, 10, 9, 2, 1, 0);
+
+        list.stream()
+                .limit(5)
+                .forEach(System.out::println);
+
+        System.out.println();
+        list.stream()
+                .sorted()
+                .limit(3)
+                .forEach(System.out::println);
+
+        System.out.println();
+        list.stream()
+                .sorted()
+                .distinct()
+                .limit(3)
+                .forEach(System.out::println);
+
+        System.out.println();
+        list.stream()
+                .sorted((a, b) -> b - a)
+                .distinct()
+                .limit(3)
+                .forEach(System.out::println);
+
+        System.out.println();
+        list.stream()
+                .sorted(Comparator.reverseOrder())
+                .distinct()
+                .limit(3)
+                .forEach(System.out::println);
+
+
+    }
+}
