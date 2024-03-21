dd12c1cba1eef1a723f5d165a3549907cc549769
sebaek <sebaek@gmail.com>
2024-03-21 09:36:58 +0900

    leetcode 1748

---
+++ src/ch15/lecture/p5map/C02LeetCode1748.java
@@ -0,0 +1,31 @@
+package ch15.lecture.p5map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C02LeetCode1748 {
+}
+
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int num : nums) {
+            Integer value = map.get(num);
+            if (value == null) {
+                map.put(num, 1);
+            } else {
+                map.put(num, value + 1);
+            }
+        }
+
+        int result = 0;
+        var entries = map.entrySet();
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            if (entry.getValue() == 1) {
+                result += entry.getKey();
+            }
+        }
+
+        return result;
+    }
+}
