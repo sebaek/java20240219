ac0e05005bdd1b788c55ed6a6762a41eb932cf37
sebaek <sebaek@gmail.com>
2024-03-21 17:07:27 +0900

    leetcode 1748

--- src/ch15/lecture/p5map/C02LeetCode1748.java
+++ src/ch15/lecture/p5map/C02LeetCode1748.java
@@ -1,7 +1,9 @@
 package ch15.lecture.p5map;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class C02LeetCode1748 {
 }
@@ -27,5 +29,32 @@ class Solution {
         }
 
         return result;
+    }
+}
+
+
+class Solution2 {
+    public int sumOfUnique(int[] nums) {
+        Set<Integer> set1 = new HashSet<>(); // 모두 담고
+        Set<Integer> set2 = new HashSet<>(); // 중복인 것 담고
+
+        for (int num : nums) {
+            if (!set1.add(num)) {
+                set2.add(num);
+            }
+        }
+
+        int sum1 = 0;
+        for (int num : set1) {
+            sum1 += num;
+        }
+
+        int sum2 = 0;
+        for (int num : set2) {
+            sum2 += num;
+        }
+
+        return sum1 - sum2;
+
     }
 }
