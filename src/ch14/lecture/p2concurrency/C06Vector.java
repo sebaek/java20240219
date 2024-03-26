ba6d9f45b4a3fea9017ec29f2f53058b39a27d86
sebaek <sebaek@gmail.com>
2024-03-26 15:10:54 +0900

    vector, synchonizedList

---
+++ src/ch14/lecture/p2concurrency/C06Vector.java
@@ -0,0 +1,27 @@
+package ch14.lecture.p2concurrency;
+
+import java.util.List;
+import java.util.Vector;
+
+public class C06Vector {
+    public static void main(String[] args) throws InterruptedException {
+        List<String> vector = new Vector<>();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 30000; i++) {
+                vector.add("a");
+                vector.remove("a");
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 30000; i++) {
+                vector.add("b");
+                vector.remove("b");
+            }
+        });
+        t1.start();
+        t2.start();
+        t2.join();
+        t2.join();
+        System.out.println("vector = " + vector);
+    }
+}
