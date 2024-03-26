0eb8e0ed87d2b0553faeccdeb5097a27bfe08fa8
sebaek <sebaek@gmail.com>
2024-03-26 14:49:07 +0900

    arraylist

---
+++ src/ch14/lecture/p2concurrency/C05ArrayList.java
@@ -0,0 +1,29 @@
+package ch14.lecture.p2concurrency;
+
+import java.util.ArrayList;
+
+public class C05ArrayList {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<String> list = new ArrayList<>();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 30000; i++) {
+                list.add("a");
+                list.remove("a");
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 30000; i++) {
+                list.add("b");
+                list.remove("b");
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(list);
+    }
+}
