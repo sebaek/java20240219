3db5d7077caf32b1494ec51fa82a31091e0f9098
sebaek <sebaek@gmail.com>
2024-03-27 16:42:25 +0900

    보조 스트림

---
+++ src/ch18/sec06/CharacterConvertStreamExample.java
@@ -0,0 +1,30 @@
+package ch18.sec06;
+
+import java.io.*;
+
+public class CharacterConvertStreamExample {
+    public static void main(String[] args) throws Exception {
+        write("문자 변환 스트림을 사용합니다.");
+        String data = read();
+        System.out.println("data = " + data);
+    }
+
+    private static String read() throws Exception {
+        InputStream is = new FileInputStream("temp/test.txt");
+        Reader reader = new InputStreamReader(is);
+        char[] data = new char[100];
+        int num = reader.read(data);
+        reader.close();
+        String str = new String(data, 0, num);
+        return str;
+    }
+
+
+    private static void write(String str) throws Exception {
+        OutputStream os = new FileOutputStream("temp/test.txt");
+        Writer writer = new OutputStreamWriter(os);
+        writer.write(str);
+        writer.flush();
+        writer.close();
+    }
+}
