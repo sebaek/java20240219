5ec5c79e2de08c61f4a63a53b424dad42c0c68dc
sebaek <sebaek@gmail.com>
2024-03-27 15:29:09 +0900

    copy text file

---
+++ src/ch18/lecture/p4reader/C05CopyCode.java
@@ -0,0 +1,16 @@
+package ch18.lecture.p4reader;
+
+public class C05CopyCode {
+    public static void main(String[] args) {
+        // todo: 텍스트 파일 복사
+        // C05CopyCode.java 파일을
+        // temp/C05CopyCode.txt 파일로 복사
+
+        // Reader, Writer 활용
+
+        String src = "src/ch18/lecture/p4reader/C05CopyCode.java";
+        String des = "temp/C05CopyCode.txt";
+
+
+    }
+}
