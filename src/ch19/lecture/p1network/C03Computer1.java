7793bdec4f2909e4feaafec05003fbc6513315cc
sebaek <sebaek@gmail.com>
2024-03-29 10:43:30 +0900

    파일 보내기

---
+++ src/ch19/lecture/p1network/C03Computer1.java
@@ -0,0 +1,23 @@
+package ch19.lecture.p1network;
+
+public class C03Computer1 {
+    public static void main(String[] args) {
+        // ServerSocket 만들기
+
+        // 연결 기다린 후 
+        // 연결되면 Socket 만들기
+
+        // Socket에서 OutputStream 꺼내기
+        // BufferedOutputStream 연결
+
+        // 파일은 아무거나
+        // FileInputStream 만들기
+        // BufferedInputStream 연결
+
+        // BufferedInputStream으로 읽은 데이터
+        // BufferedOutputStream으로 출력
+        // 마지막에 flush()
+
+        // 끝
+    }
+}
---
+++ src/ch19/lecture/p1network/C03Computer2.java
@@ -0,0 +1,20 @@
+package ch19.lecture.p1network;
+
+public class C03Computer2 {
+    public static void main(String[] args) {
+        // 서버에 연결해서 Socket 만들기
+
+        // Socket에서 InputStream 꺼내기
+        // BufferedInputStream 연결
+
+        // 파일명 아무거나
+        // FileOutputStream 만들기
+        // BufferedOutputStream 연결
+
+        // BufferedInputStream에서 읽은 데이터
+        // BufferedOutputStream으로 쓰기
+        // 마지막에 flush()
+
+        // 끝.
+    }
+}
