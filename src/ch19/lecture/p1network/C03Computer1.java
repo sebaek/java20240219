ce366512b5c400390ed96a9a58ce70fdae935fbf
sebaek <sebaek@gmail.com>
2024-03-29 12:10:10 +0900

    file 전송

--- src/ch19/lecture/p1network/C03Computer1.java
+++ src/ch19/lecture/p1network/C03Computer1.java
@@ -1,22 +1,45 @@
 package ch19.lecture.p1network;
 
-public class C03Computer1 {
-    public static void main(String[] args) {
-        // ServerSocket 만들기
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class C03Computer1 {
+    public static void main(String[] args) throws IOException {
+        // ServerSocket 만들기
+        ServerSocket serverSocket = new ServerSocket(9000);
         // 연결 기다린 후 
         // 연결되면 Socket 만들기
+        Socket socket = serverSocket.accept();
 
-        // Socket에서 OutputStream 꺼내기
-        // BufferedOutputStream 연결
+        try (socket) {
+            // Socket에서 OutputStream 꺼내기
+            // BufferedOutputStream 연결
+            BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
 
-        // 파일은 아무거나
-        // FileInputStream 만들기
-        // BufferedInputStream 연결
+            // 파일은 아무거나
+            // FileInputStream 만들기
+            // BufferedInputStream 연결
+            FileInputStream fis = new FileInputStream("temp/cat.png");
+            BufferedInputStream bis = new BufferedInputStream(fis);
 
-        // BufferedInputStream으로 읽은 데이터
-        // BufferedOutputStream으로 출력
-        // 마지막에 flush()
+            try (bos; fis; bis) {
+
+                // BufferedInputStream으로 읽은 데이터
+                // BufferedOutputStream으로 출력
+                byte[] data = new byte[1024];
+                int len = 0;
+                while ((len = bis.read(data)) != -1) {
+                    bos.write(data, 0, len);
+                }
+
+                bos.flush();
+            }
+            // 마지막에 flush()
+        }
 
         // 끝
     }
--- src/ch19/lecture/p1network/C03Computer2.java
+++ src/ch19/lecture/p1network/C03Computer2.java
@@ -1,19 +1,44 @@
 package ch19.lecture.p1network;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class C03Computer2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 서버에 연결해서 Socket 만들기
+        Socket socket = new Socket("172.30.1.73", 9000);
 
-        // Socket에서 InputStream 꺼내기
-        // BufferedInputStream 연결
+        try (socket) {
 
-        // 파일명 아무거나
-        // FileOutputStream 만들기
-        // BufferedOutputStream 연결
+            // Socket에서 InputStream 꺼내기
+            // BufferedInputStream 연결
+            BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
 
-        // BufferedInputStream에서 읽은 데이터
-        // BufferedOutputStream으로 쓰기
-        // 마지막에 flush()
+            // 파일명 아무거나
+            // FileOutputStream 만들기
+            // BufferedOutputStream 연결
+            String file = "temp/download.png";
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            try (bis; fos; bos) {
+
+                // BufferedInputStream에서 읽은 데이터
+                // BufferedOutputStream으로 쓰기
+                byte[] data = new byte[300];
+                int len = 0;
+
+                while ((len = bis.read(data)) != -1) {
+                    bos.write(data, 0, len);
+                }
+                // 마지막에 flush()
+                bos.flush();
+            }
+
+        }
 
         // 끝.
     }
