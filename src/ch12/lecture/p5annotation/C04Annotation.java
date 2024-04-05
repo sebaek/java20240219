bae0917d7649beeb0b2ea3d0e941fa729dd50229
sebaek <sebaek@gmail.com>
2024-04-05 09:35:28 +0900

    annotation 사용 위치 제한

---
+++ src/ch12/lecture/p5annotation/C04Annotation.java
@@ -0,0 +1,13 @@
+package ch12.lecture.p5annotation;
+
+@MyAnnotation4
+public class C04Annotation {
+
+    @MyAnnotation4
+    public void method1(@MyAnnotation4 String param) {
+
+    }
+}
+// MyAnnotation4 작성
+// TYPE, METHOD, PARAMETER 에만 사용 가능하도록 제한
+
